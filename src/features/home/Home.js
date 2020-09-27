f5209010d33a248d3a92249045134e518276bd9b
aleenareji7 <aleenareji7@gmail.com>
2020-09-27 22:22:24 +0530

    Initial commit

---
+++ src/features/home/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './redux/actions';
+
+export class Home extends Component {
+  static propTypes = {
+    home: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired,
+  };
+
+  render() {
+    return (
+      <div className="home-home">
+       <h2>React application</h2>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    home: state.home,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({ ...actions }, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
